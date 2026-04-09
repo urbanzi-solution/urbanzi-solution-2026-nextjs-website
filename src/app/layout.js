3845160452e330b0303b2ba00b44b0c2f286616f
Abhijith <abhijithjd.personal@gmail.com>
2026-04-09 22:22:21 +0530

    feat: initialize root layout with global components, metadata, and Google Analytics integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import WhatsappButton from "@/components/WhatsappButton"
@@ -42,6 +43,18 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#000002] text-white`}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HH76NSBHCW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HH76NSBHCW');
+          `}
+        </Script>
         <LeadProvider>
           <Navbar />
           {children}
